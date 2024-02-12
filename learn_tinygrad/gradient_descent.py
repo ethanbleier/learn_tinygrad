65dcaa49c461461c19043ddc999ba501c04f0dda
ethan bleier <ebleier4@gmail.com>
2024-02-11 20:06:51 -0800

    used tinygrad optimizer Adam

--- learn_tinygrad/gradient_descent.py
+++ learn_tinygrad/gradient_descent.py
@@ -1,6 +1,7 @@
 from tinygrad.tensor import Tensor
 import numpy as np
 import matplotlib.pyplot as plt
+from tinygrad.nn.optim import Adam
 
 def f(x):
 	if not isinstance(x, Tensor):
@@ -12,4 +13,16 @@ plt.plot(data, f(data).numpy())
 
 x = Tensor([-1.3])
 plt.plot(x.numpy(), f(x).numpy(), 'go')
-plt.show()
+
+adam = Adam([x], lr=0.001)
+
+y_exp = Tensor([0])
+for _ in range(2000):
+	loss = -(y_exp ** 2 - f(x) ** 2).mean()
+	loss.backward()
+	adam.step()
+	print(f"Loss: {loss.numpy()}")
+	if loss.numpy() < 0.3:
+		break
+
+print(f"Final value of x: {x.numpy()}")
