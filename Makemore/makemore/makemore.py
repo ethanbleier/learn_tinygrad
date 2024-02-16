c81a6e00e856c84efff23cecb61958ae44ef2fe3
ethan bleier <ebleier4@gmail.com>
2024-02-16 11:32:40 -0800

    sorting data by bigrams

--- Makemore/makemore/makemore.py
+++ Makemore/makemore/makemore.py
@@ -3,9 +3,20 @@
 from pprint import pprint
 
 words = open('names.txt', 'r').read().splitlines()
-# words[:10]
-for w in words[:1]:
-	for ch1, ch2 in zip(w, w[1:]):
+
+
+# this is how we get the counts of all the words of the individial bigrams
+b = {}
+for w in words:
+	chs = ['<S>'] + list(w) + ['<E>']
+	for ch1, ch2 in zip(chs, chs[1:]):
+		# count how often the combinations occur
+		bigram = (ch1, ch2)
+		b[bigram] = b.get(bigram, 0) + 1
 		print(ch1, ch2)
 
-	
+# sorting data by the count of the elements
+#  kv[1] for smallest
+# -kv[1] for largest
+sorted(b.items(), key = lambda kv: -kv[1]) 
+pprint(b)
