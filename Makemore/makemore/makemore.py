0b196193b67a80165ec851ff7e2c8d562c5aa233
ethan bleier <ebleier4@gmail.com>
2024-02-16 11:23:47 -0800

    printing tuples of entries

--- Makemore/makemore/makemore.py
+++ Makemore/makemore/makemore.py
@@ -1,5 +1,11 @@
+# simple and weak language model that takes on character and predicts the next character in a first name. simple & a good place to start.
+
 from pprint import pprint
 
 words = open('names.txt', 'r').read().splitlines()
 # words[:10]
-pprint(words)
+for w in words[:1]:
+	for ch1, ch2 in zip(w, w[1:]):
+		print(ch1, ch2)
+
+	
